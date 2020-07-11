360a1acd935c030cbd382a96ae3ecdbb39aa14a7
jason wolosonovich <jason@avaland.io>
2020-07-10 18:47:28 -0700

    REF: updated structure

--- src/greeting_service/services/__init__.py
+++
@@ -1,1 +0,0 @@
-from .greeting_generator import GreetingGenerator
--- src/greeting_service/services/greeting_generator.py
+++
@@ -1,6 +0,0 @@
-class GreetingGenerator(object):
-    '''Standard greeting library'''
-
-    def say_hello_world(self, target: str):
-        '''Says hello to target'''
-        print(f"Hello {target}!")
